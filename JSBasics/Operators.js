634b2d82ef432a01fba29fbffc9a07daf7c778d7
bhm88 <bharathph@yahoo.in>
2020-10-03 00:02:58 +0530

    basics

---
+++ JSBasics/Operators.js
@@ -0,0 +1,23 @@
+var year,yearBharat,yearJohn;
+now=2018;
+ageBharat=31;
+ageJohn=25;
+yearBharat=now-ageBharat;
+yearJohn=now-ageJohn;
+
+
+// math operator
+console.log(now + 2);
+console.log(now - 2);
+console.log(now * 2);
+console.log(now / 2);
+
+//logical operators
+
+var older=ageBharat>ageJohn;
+console.log(older)
+
+
+// type of operator
+
+console.log(typeof older)
--- JSBasics/script.js
+++ JSBasics/script.js
@@ -1,1 +1,47 @@
 console.log('hello from external js file')
+
+
+// here  in js use single quotes
+var firstName='John';
+var age=31;
+
+var fullAge=true;
+
+var job;
+
+console.log(firstName)
+console.log(age)
+console.log(fullAge)
+console.log(job)
+
+job='engineer'
+console.log(job)
+
+
+/*
+* multiline comments 
+
+*/
+
+// variable  mutation and type coersion
+
+var lastName='hiremath'
+var age=31;
+// type coersion example
+console.log(lastName + '' + age)
+
+var profession,isMarried;
+profession='developer';
+
+isMarried=true;
+// type coersion example
+console.log(lastName + '' + 'is a ' + age + 'year old ' + 'working as a ' + profession + 'is he married ?' + isMarried);
+
+//variable mutation
+age='forty one'
+profession='driver'
+alert(lastName + '' + 'is a ' + age + 'year old ' + 'working as a ' + profession + 'is he married ?' + isMarried);
+
+var locaation=prompt('wts ur location ?');
+console.log(lastName + '' + 'is a ' + age + 'year old ' + 'working as a ' + profession + 'is he married ?' + isMarried + 'his location is ' + locaation);
+
